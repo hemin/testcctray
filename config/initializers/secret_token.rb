df605d9b408ae9ea849f20797bfc90846e5ac1c7
Min He <410152019@qq.com>
2011-07-18 14:28:24 +0800

    initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testcctray::Application.config.secret_token = '5d2db0775ea68f1d4fe9b614ae0a28907f939e78ddd09439392c9886ce083d778e40a998306901617194e2bd46279039ea8b7423f1443ca5f629c087ac44f29a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Testcctray::Application.config.session_store :cookie_store, :key => '_testcctray_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Testcctray::Application.config.session_store :active_record_store
